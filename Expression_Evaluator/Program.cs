5960459651b0a43211dcb5fe0e084c9e77fc5afd
Sadeesha94 <sadishanishara12@gmail.com>
2019-11-11 17:34:29 +0530

    newcode

--- Expression_Evaluator/Program.cs
+++ Expression_Evaluator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Text.RegularExpressions;
 using Evaluator;
 
 namespace Expression_Evaluator
@@ -9,17 +11,112 @@ namespace Expression_Evaluator
     {
         static void Main(string[] args)
         {
-            ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator();
-            
 
-            Dictionary<string, double> variables = new Dictionary<string, double>();
-            variables.Add("variable1", -1);
-            variables.Add("variable2", 2);
-            variables.Add("Variable3", 4.25);
+            //int variable = 1;
+            //DataTable dt = new DataTable();
+            //var result1 = dt.Compute("variable * (2 + 3) - 2 / 8", "+variable+");
+            //Console.WriteLine("The result is" + result1);
 
-            double result = expressionEvaluator.Evaluate(variables, "sin(90) * (variable1 + Variable2) - 2 * Variable3 / 8");
+            //Dictionary<string, double> variables = new Dictionary<string, double>();
+            //variables.Add("variable1", 5);
+            //variables.Add("var", 4);
+            //variables.Add("variable2", 2);
+            //variables.Add("variable3", 4.25);
+            //string expression = "sin(90) * (variable + 1variable2) - 2 / 8";
 
-            Console.WriteLine(result);
+            //ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator(variables, expression);
+
+            //double result = expressionEvaluator.Evaluate();
+
+            //Console.WriteLine(result);
+            //Console.WriteLine(variables.Count);
+
+            ////String delimiters = "!@#$%^&*()-=_+{}|\\][:\"';?><,./ ";
+
+            String str = "sin(90) * (variable1 + variable2) - 2 / 8+var///variable3";
+
+            string[] spearator = { "+", "-", "/", "*", "(", ")", " ", "sin", "cos", "tan" };
+
+            // using the method 
+            String[] strlist = str.Split(spearator, StringSplitOptions.RemoveEmptyEntries);
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+
+            var regex = new Regex(@"^[!@#$%^&A-Za-z_-][\w&\-\!\@\#\$\%\^]*$");
+            int value;
+            int i = 0;
+
+
+            foreach (String s in strlist)
+            {
+                if (variables.ContainsKey(s))
+                {
+                    count1++;
+                }
+
+                if (!int.TryParse(s, out value))
+                {
+                    count3++;
+                }
+
+            }
+
+            string[] word = new string[count3];
+
+            foreach (String a in strlist)
+            {
+                if (!int.TryParse(a, out value))
+                {
+                    word[i] = a;
+                    i++;
+                }
+
+            }
+            foreach (String a in word)
+            {
+                if (regex.IsMatch(a))
+                {
+                    count2++;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Variable Name");
+                }
+            }
+
+            Console.WriteLine(count1);
+            Console.WriteLine(count2);
+
+            foreach (String s in strlist)
+            {
+                Console.WriteLine(s);
+            }
+
+            //if (count2 != count1)
+            //{
+            //    Console.WriteLine("Ooooops...Expression has Undefined Variables...!!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Expression is OK");
+            //}
+
+
+
+
+
+
+
+            //double? a = null;
+            //Console.WriteLine(a);
+
+            //if (variables.ContainsValue())
+            //    Console.WriteLine("Value : India is present.");
+
+            //else
+            //    Console.WriteLine("Value : India is not present.");
+
         }
     }
 }
